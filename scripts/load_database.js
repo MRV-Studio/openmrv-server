29ec3f01cb2196b3b2102a5bba7e6e28e5fb74ed
tsondru <k.tsondru.tarchin@pm.me>
2022-06-24 09:02:28 -0600

    correctly typed import

--- scripts/load_database.js
+++ scripts/load_database.js
@@ -2,7 +2,7 @@
 var myCursor = db.sample_weather_source.find().sort ( { time : 1 } );
 while (myCursor.hasNext()) {
    var doc = myCursor.next();
-   db.weather.insertOne({ "ts": ISODate(doc.time), "metadata": { "station": "Moratalaz", "region": "Madrid" },
+   db.weather.insertOne({ "ts": doc.time, "metadata": { "station": "Moratalaz", "region": "Madrid" },
     "temperature": doc.temperature, "humidity": doc.humidity, "pressure": doc.barometric_pressure,
      "wind_speed": doc.wind_speed, "wind_direction": doc.wind_direction, "precipitation": doc.precipitation,
    "solar_radiation": doc.solar_radiation });
