790c07cc57bca51d6e4ddb21525cc79945f39101
tsondru <k.tsondru.tarchin@pm.me>
2022-07-18 13:47:13 -0600

    sample data adjustments

--- scripts/load_database.js
+++ scripts/load_database.js
@@ -4,9 +4,11 @@ var myCursor = db.sample_weather_source.find().sort ( { time : 1 } );
 while (myCursor.hasNext()) {
    var doc = myCursor.next();
    db.weather.insertOne({ "_id": nanoid(), "ts": ISODate(doc.time), "metadata": { "station": "Moratalaz", "region": "Madrid" },
+   "location": { "type": "Point", "coordinates": [-3.7025600, 40.4165000] }, 
     "temperature": doc.temperature, "humidity": doc.humidity, "pressure": doc.barometric_pressure,
      "wind_speed": doc.wind_speed, "wind_direction": doc.wind_direction, "precipitation": doc.precipitation,
    "solar_radiation": doc.solar_radiation });
 
    printjson(doc);
 }
+db.weather.createIndex( { location : "2dsphere" } )
